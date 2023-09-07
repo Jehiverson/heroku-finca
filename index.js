5081118e868ae38646d71b46c98f8f3332d5d9ec
Jehiverson <jehiverson@MacBook-Air-de-Temporal.local>
2023-09-07 11:21:21 -0600

    make it better

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const Configuration = require('./Configuration');
 const cybersourceRestApi =require('cybersource-rest-client');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
 
 async function esperarXSegundos(segundos) {
     const esperaMilisegundos = segundos * 1000;
